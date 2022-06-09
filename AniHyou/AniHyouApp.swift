dd0b89fbcea6259c908986eb357bdc4961b05efc
Axel <axiel7lopez@gmail.com>
2022-06-09 08:45:09 +0200

    Initial Commit

---
+++ AniHyou/AniHyouApp.swift
@@ -0,0 +1,17 @@
+//
+//  AniHyouApp.swift
+//  AniHyou
+//
+//  Created by Axel Lopez on 9/6/22.
+//
+
+import SwiftUI
+
+@main
+struct AniHyouApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AniHyou/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AniHyou
+//
+//  Created by Axel Lopez on 9/6/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
