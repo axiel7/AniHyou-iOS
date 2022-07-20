238fdc466ae4b7feeb2b6eba5b330cf3e6ed9545
Axel <axiel7lopez@gmail.com>
2022-07-20 19:49:21 +0200

    Show staff in grid

--- AniHyou/Views/MediaDetails/MediaCharactersAndStaffView.swift
+++ AniHyou/Views/MediaDetails/MediaCharactersAndStaffView.swift
@@ -10,46 +10,38 @@ import SwiftUI
 struct MediaCharactersAndStaffView: View {
     
     @ObservedObject var viewModel: MediaDetailsViewModel
+    private let gridRows = [
+        GridItem(.fixed(StaffView.imageSize), alignment: .leading),
+        GridItem(.fixed(StaffView.imageSize), alignment: .leading)
+    ]
     
     var body: some View {
-        VStack(alignment: .leading) {
-            
-            Text("Staff")
-                .font(.title3)
-                .bold()
-                .padding(.leading)
-            ZStack {
-                if viewModel.mediaCharactersAndStaff == nil {
-                    ProgressView()
-                }
+        if viewModel.mediaCharactersAndStaff != nil {
+            VStack(alignment: .leading) {
+                
+                Text("Staff")
+                    .font(.title3)
+                    .bold()
+                    .padding(.leading)
+                
                 ScrollView(.horizontal, showsIndicators: false) {
-                    LazyHStack {
+                    LazyHGrid(rows: gridRows, spacing: 16) {
                         ForEach(viewModel.mediaCharactersAndStaff?.staff?.edges ?? [], id: \.?.node?.id) {
                             if let staff = $0 {
                                 StaffView(staff: staff.fragments.mediaStaff)
-                                    .padding(.trailing)
                             }
                         }
-                    }
+                    }//:HGrid
                     .padding(.leading)
-                }
-            }
-            .frame(height: StaffView.imageSize)
-            .padding(.bottom)
-            
-            Text("Characters")
-                .font(.title3)
-                .bold()
-                .padding(.leading)
-            ZStack(alignment: .center) {
-                if viewModel.mediaCharactersAndStaff == nil {
-                    HStack {
-                        Spacer()
-                        ProgressView()
-                        Spacer()
-                    }
-                    .padding(.top)
-                }
+                }//:HScrollView
+                .frame(minHeight: StaffView.imageSize, maxHeight: StaffView.imageSize * 2 + 8)
+                .padding(.bottom)
+                
+                Text("Characters")
+                    .font(.title3)
+                    .bold()
+                    .padding(.leading)
+                
                 LazyVStack {
                     ForEach(viewModel.mediaCharactersAndStaff?.characters?.edges ?? [], id: \.?.node?.id) {
                         if let character = $0 {
@@ -57,10 +49,17 @@ struct MediaCharactersAndStaffView: View {
                                 .padding(.bottom)
                         }
                     }
-                }
+                }//:VStack
+                .padding(.leading)
+                .padding(.trailing)
+            }//:VStack
+        } else {
+            HStack {
+                Spacer()
+                ProgressView()
+                    .padding()
+                Spacer()
             }
-            .padding(.leading)
-            .padding(.trailing)
         }
     }
 }
