827ae204b84f8aaf26370ea2bf4b4f26f3efd103
Axel <axiel7lopez@gmail.com>
2022-08-08 17:28:20 +0200

    Refactor

--- AniHyou/Screens/MediaList/MediaListStatusView.swift
+++ AniHyou/Screens/MediaList/MediaListStatusView.swift
@@ -7,47 +7,6 @@
 
 import SwiftUI
 
-extension MediaListStatus {
-    var localizedName: String {
-        switch self {
-        case .current:
-            return "Current"
-        case .completed:
-            return "Completed"
-        case .paused:
-            return "Paused"
-        case .planning:
-            return "Planning"
-        case .dropped:
-            return "Dropped"
-        case .repeating:
-            return "Repeating"
-        default:
-            return "Unknown"
-        }
-    }
-    
-    /// System symbol name representing this status
-    var systemIcon: String {
-        switch self {
-        case .current:
-            return "play.circle"
-        case .completed:
-            return "checkmark.circle"
-        case .paused:
-            return "pause.circle"
-        case .planning:
-            return "clock"
-        case .dropped:
-            return "trash"
-        case .repeating:
-            return "repeat"
-        default:
-            return "questionmark.circle"
-        }
-    }
-}
-
 struct MediaListStatusView: View {
     
     var mediaType: MediaType
