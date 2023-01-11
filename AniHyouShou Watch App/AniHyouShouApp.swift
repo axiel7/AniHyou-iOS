d97f03104f7bc297c78a987777cec3062803a2f4
Axel <axiel7lopez@gmail.com>
2023-01-11 16:11:18 +0100

    Add WatchOS app

---
+++ AniHyouShou Watch App/AniHyouShouApp.swift
@@ -0,0 +1,17 @@
+//
+//  AniHyouShouApp.swift
+//  AniHyouShou Watch App
+//
+//  Created by Axel Lopez on 11/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct AniHyouShou_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AniHyouShou Watch App/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  AniHyouShou Watch App
+//
+//  Created by Axel Lopez on 11/01/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
