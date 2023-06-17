9b5f43ab6c9a4376dc0eccf42876adb700a20927
Axel <axiel7lopez@gmail.com>
2023-06-17 14:53:07 +0900

    Home: divide sections in variables

--- AniHyou/Screens/Home/Views/HomeView.swift
+++ AniHyou/Screens/Home/Views/HomeView.swift
@@ -16,189 +16,16 @@ struct HomeView: View {
         NavigationView {
             ScrollView(.vertical, showsIndicators: false) {
                 LazyVStack(alignment: .leading) {
-                    // MARK: - Airing
-                    Group {
-                        HStack(alignment: .center) {
-                            Text("Airing Soon")
-                                .font(.title2)
-                                .bold()
-                            Spacer()
-                            NavigationLink("See All", destination: CalendarAnimeView())
-                        }
-                        .padding(.horizontal)
-                        .padding(.top, 8)
-                        ZStack {
-                            if viewModel.airingAnimes.count == 0 {
-                                Text("No anime for today\n(*´-`)")
-                                    .multilineTextAlignment(.center)
-                                    .frame(alignment: .center)
-                            }
-                            
-                            ScrollView(.horizontal, showsIndicators: false) {
-                                LazyHStack {
-                                    ForEach(viewModel.airingAnimes, id: \.?.media?.id) {
-                                        if let item = $0 {
-                                            NavigationLink(destination: MediaDetailsView(mediaId: item.media!.id)) {
-                                                HListItemWithSubtitleView(title: item.media?.title?.userPreferred, subtitle: "Airing in \(item.timeUntilAiring.secondsToLegibleText())", imageUrl: item.media?.coverImage?.large, meanScore: item.media?.meanScore)
-                                                    .padding(.leading, 8)
-                                                    .frame(width: 280, alignment: .leading)
-                                            }
-                                        }
-                                    }
-                                }//:HStack
-                                .padding(.leading, 8)
-                            }//:HScrollView
-                            .frame(height: 145)
-                            .onAppear {
-                                viewModel.getAiringAnimes()
-                            }
-                        }//:ZStack
-                        Divider()
-                    }
                     
-                    // MARK: - Season
-                    Group {
-                        HStack(alignment: .center) {
-                            Text("\(viewModel.nowAnimeSeason.season.localizedName) \(String(viewModel.nowAnimeSeason.year))")
-                                .font(.title2)
-                                .bold()
-                            Spacer()
-                            NavigationLink("See All", destination: AnimeSeasonListView(season: viewModel.nowAnimeSeason.season))
-                        }
-                        .padding(.horizontal)
-                        .padding(.top, 8)
-                        ZStack {
-                            if viewModel.seasonAnimes.count == 0 {
-                                ProgressView()
-                            }
-                            ScrollView(.horizontal, showsIndicators: false) {
-                                LazyHStack(alignment: .top) {
-                                    ForEach(viewModel.seasonAnimes, id: \.?.id) {
-                                        if let item = $0 {
-                                            NavigationLink(destination: MediaDetailsView(mediaId: item.id)) {
-                                                VListItemView(title: item.title?.userPreferred ?? "", imageUrl: item.coverImage?.large, meanScore: item.meanScore)
-                                                    .padding(.trailing, 4)
-                                            }
-                                        }
-                                    }
-                                }//:HStack
-                                .padding(.leading, 14)
-                            }//:HScrollView
-                            .frame(minHeight: 180)
-                            .onAppear {
-                                viewModel.getSeasonAnimes()
-                            }
-                        }//:ZStack
-                        Divider()
-                    }
+                    airingSoon
                     
-                    //MARK: - Trending Anime
-                    Group {
-                        HStack(alignment: .center) {
-                            Text("Trending Anime")
-                                .font(.title2)
-                                .bold()
-                            Spacer()
-                            NavigationLink("See All", destination: TrendingListView(viewModel: viewModel, mediaType: .anime))
-                        }
-                        .padding(.horizontal)
-                        .padding(.top, 8)
-                        ZStack {
-                            if viewModel.trendingAnimes.count == 0 {
-                                ProgressView()
-                            }
-                            ScrollView(.horizontal, showsIndicators: false) {
-                                LazyHStack(alignment: .top) {
-                                    ForEach(viewModel.trendingAnimes, id: \.?.id) {
-                                        if let item = $0 {
-                                            NavigationLink(destination: MediaDetailsView(mediaId: item.id)) {
-                                                VListItemView(title: item.title?.userPreferred ?? "", imageUrl: item.coverImage?.large, meanScore: item.meanScore)
-                                                    .padding(.trailing, 4)
-                                            }
-                                        }
-                                    }
-                                }//:HStack
-                                .padding(.leading, 14)
-                            }//:HScrollView
-                            .frame(minHeight: 180)
-                            .onAppear {
-                                viewModel.getTrendingAnimes()
-                            }
-                        }//:ZStack
-                        Divider()
-                    }
+                    thisSeason
                     
-                    //MARK: - Next Season
-                    Group {
-                        HStack(alignment: .center) {
-                            Text("Next Season")
-                                .font(.title2)
-                                .bold()
-                            Spacer()
-                            NavigationLink("See All", destination: AnimeSeasonListView(season: viewModel.nextAnimeSeason.season, selectedYear: viewModel.nextAnimeSeason.year))
-                        }
-                        .padding(.horizontal)
-                        .padding(.top, 8)
-                        ZStack {
-                            if viewModel.nextSeasonAnimes.count == 0 {
-                                ProgressView()
-                            }
-                            ScrollView(.horizontal, showsIndicators: false) {
-                                LazyHStack(alignment: .top) {
-                                    ForEach(viewModel.nextSeasonAnimes, id: \.?.id) {
-                                        if let item = $0 {
-                                            NavigationLink(destination: MediaDetailsView(mediaId: item.id)) {
-                                                VListItemView(title: item.title?.userPreferred ?? "", imageUrl: item.coverImage?.large, meanScore: item.meanScore)
-                                                    .padding(.trailing, 4)
-                                            }
-                                        }
-                                    }
-                                }//:HStack
-                                .padding(.leading, 14)
-                            }//:HScrollView
-                            .frame(minHeight: 180)
-                            .onAppear {
-                                viewModel.getNextSeasonAnimes()
-                            }
-                        }//:ZStack
-                        Divider()
-                    }
+                    trendingAnime
                     
-                    //MARK: - Trending manga
-                    Group {
-                        HStack(alignment: .center) {
-                            Text("Trending Manga")
-                                .font(.title2)
-                                .bold()
-                            Spacer()
-                            NavigationLink("See All", destination: TrendingListView(viewModel: viewModel, mediaType: .manga))
-                        }
-                        .padding(.horizontal)
-                        .padding(.top, 8)
-                        ZStack {
-                            if viewModel.trendingManga.count == 0 {
-                                ProgressView()
-                            }
-                            ScrollView(.horizontal, showsIndicators: false) {
-                                LazyHStack(alignment: .top) {
-                                    ForEach(viewModel.trendingManga, id: \.?.id) {
-                                        if let item = $0 {
-                                            NavigationLink(destination: MediaDetailsView(mediaId: item.id)) {
-                                                VListItemView(title: item.title?.userPreferred ?? "", imageUrl: item.coverImage?.large, meanScore: item.meanScore)
-                                                    .padding(.trailing, 4)
-                                            }
-                                        }
-                                    }
-                                }//:HStack
-                                .padding(.leading, 14)
-                            }//:HScrollView
-                            .frame(minHeight: 180)
-                            .onAppear {
-                                viewModel.getTrendingManga()
-                            }
-                        }//:ZStack
-                        .padding(.bottom)
-                    }
+                    nextSeason
+                    
+                    trendingManga
                 }//:VStack
             }//:VScrollView
             .navigationTitle("Home")
@@ -217,6 +44,190 @@ struct HomeView: View {
             }
         }//:NavigationView
         .navigationViewStyle(.stack)
+    }
+    
+    @ViewBuilder
+    var airingSoon: some View {
+        HStack(alignment: .center) {
+            Text("Airing Soon")
+                .font(.title2)
+                .bold()
+            Spacer()
+            NavigationLink("See All", destination: CalendarAnimeView())
+        }
+        .padding(.horizontal)
+        .padding(.top, 8)
+        ZStack {
+            if viewModel.airingAnimes.count == 0 {
+                Text("No anime for today\n(*´-`)")
+                    .multilineTextAlignment(.center)
+                    .frame(alignment: .center)
+            }
+            
+            ScrollView(.horizontal, showsIndicators: false) {
+                LazyHStack {
+                    ForEach(viewModel.airingAnimes, id: \.?.mediaId) {
+                        if let item = $0 {
+                            NavigationLink(destination: MediaDetailsView(mediaId: item.mediaId)) {
+                                HListItemWithSubtitleView(title: item.media?.title?.userPreferred, subtitle: "Airing in \(item.timeUntilAiring.secondsToLegibleText())", imageUrl: item.media?.coverImage?.large, meanScore: item.media?.meanScore)
+                                    .padding(.leading, 8)
+                                    .frame(width: 280, alignment: .leading)
+                            }
+                        }
+                    }
+                }//:HStack
+                .padding(.leading, 8)
+            }//:HScrollView
+            .frame(height: 145)
+            .onAppear {
+                viewModel.getAiringAnimes()
+            }
+        }//:ZStack
+        Divider()
+    }
+    
+    @ViewBuilder
+    var thisSeason: some View {
+        HStack(alignment: .center) {
+            Text("\(viewModel.nowAnimeSeason.season.localizedName) \(String(viewModel.nowAnimeSeason.year))")
+                .font(.title2)
+                .bold()
+            Spacer()
+            NavigationLink("See All", destination: AnimeSeasonListView(season: viewModel.nowAnimeSeason.season))
+        }
+        .padding(.horizontal)
+        .padding(.top, 8)
+        ZStack {
+            if viewModel.seasonAnimes.count == 0 {
+                ProgressView()
+            }
+            ScrollView(.horizontal, showsIndicators: false) {
+                LazyHStack(alignment: .top) {
+                    ForEach(viewModel.seasonAnimes, id: \.?.id) {
+                        if let item = $0 {
+                            NavigationLink(destination: MediaDetailsView(mediaId: item.id)) {
+                                VListItemView(title: item.title?.userPreferred ?? "", imageUrl: item.coverImage?.large, meanScore: item.meanScore)
+                                    .padding(.trailing, 4)
+                            }
+                        }
+                    }
+                }//:HStack
+                .padding(.leading, 14)
+            }//:HScrollView
+            .frame(minHeight: 180)
+            .onAppear {
+                viewModel.getSeasonAnimes()
+            }
+        }//:ZStack
+        Divider()
+    }
+    
+    @ViewBuilder
+    var trendingAnime: some View {
+        HStack(alignment: .center) {
+            Text("Trending Anime")
+                .font(.title2)
+                .bold()
+            Spacer()
+            NavigationLink("See All", destination: TrendingListView(viewModel: viewModel, mediaType: .anime))
+        }
+        .padding(.horizontal)
+        .padding(.top, 8)
+        ZStack {
+            if viewModel.trendingAnimes.count == 0 {
+                ProgressView()
+            }
+            ScrollView(.horizontal, showsIndicators: false) {
+                LazyHStack(alignment: .top) {
+                    ForEach(viewModel.trendingAnimes, id: \.?.id) {
+                        if let item = $0 {
+                            NavigationLink(destination: MediaDetailsView(mediaId: item.id)) {
+                                VListItemView(title: item.title?.userPreferred ?? "", imageUrl: item.coverImage?.large, meanScore: item.meanScore)
+                                    .padding(.trailing, 4)
+                            }
+                        }
+                    }
+                }//:HStack
+                .padding(.leading, 14)
+            }//:HScrollView
+            .frame(minHeight: 180)
+            .onAppear {
+                viewModel.getTrendingAnimes()
+            }
+        }//:ZStack
+        Divider()
+    }
+    
+    @ViewBuilder
+    var nextSeason: some View {
+        HStack(alignment: .center) {
+            Text("Next Season")
+                .font(.title2)
+                .bold()
+            Spacer()
+            NavigationLink("See All", destination: AnimeSeasonListView(season: viewModel.nextAnimeSeason.season, selectedYear: viewModel.nextAnimeSeason.year))
+        }
+        .padding(.horizontal)
+        .padding(.top, 8)
+        ZStack {
+            if viewModel.nextSeasonAnimes.count == 0 {
+                ProgressView()
+            }
+            ScrollView(.horizontal, showsIndicators: false) {
+                LazyHStack(alignment: .top) {
+                    ForEach(viewModel.nextSeasonAnimes, id: \.?.id) {
+                        if let item = $0 {
+                            NavigationLink(destination: MediaDetailsView(mediaId: item.id)) {
+                                VListItemView(title: item.title?.userPreferred ?? "", imageUrl: item.coverImage?.large, meanScore: item.meanScore)
+                                    .padding(.trailing, 4)
+                            }
+                        }
+                    }
+                }//:HStack
+                .padding(.leading, 14)
+            }//:HScrollView
+            .frame(minHeight: 180)
+            .onAppear {
+                viewModel.getNextSeasonAnimes()
+            }
+        }//:ZStack
+        Divider()
+    }
+    
+    @ViewBuilder
+    var trendingManga: some View {
+        HStack(alignment: .center) {
+            Text("Trending Manga")
+                .font(.title2)
+                .bold()
+            Spacer()
+            NavigationLink("See All", destination: TrendingListView(viewModel: viewModel, mediaType: .manga))
+        }
+        .padding(.horizontal)
+        .padding(.top, 8)
+        ZStack {
+            if viewModel.trendingManga.count == 0 {
+                ProgressView()
+            }
+            ScrollView(.horizontal, showsIndicators: false) {
+                LazyHStack(alignment: .top) {
+                    ForEach(viewModel.trendingManga, id: \.?.id) {
+                        if let item = $0 {
+                            NavigationLink(destination: MediaDetailsView(mediaId: item.id)) {
+                                VListItemView(title: item.title?.userPreferred ?? "", imageUrl: item.coverImage?.large, meanScore: item.meanScore)
+                                    .padding(.trailing, 4)
+                            }
+                        }
+                    }
+                }//:HStack
+                .padding(.leading, 14)
+            }//:HScrollView
+            .frame(minHeight: 180)
+            .onAppear {
+                viewModel.getTrendingManga()
+            }
+        }//:ZStack
+        .padding(.bottom)
     }
 }
 
