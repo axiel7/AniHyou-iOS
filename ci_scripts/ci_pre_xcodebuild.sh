28191f9d37bcac6cd18b99ca7a691f26e5873f02
axiel7 <axiel7lopez@gmail.com>
2023-07-26 13:23:23 +0900

    fix script path

--- ci_scripts/ci_pre_xcodebuild.sh
+++ ci_scripts/ci_pre_xcodebuild.sh
@@ -7,8 +7,9 @@
 #  
 
 # create Config.xcconfig file with the client id for Xcode Cloud builds
-if [[ ! -z "${ANILIST_CLIENT_ID}" ]]; then
-    cd AniHyou
+if [[ -n $ANILIST_CLIENT_ID ]];
+then
+    cd ../AniHyou
     touch Config.xcconfig
     echo "ANILIST_CLIENT_ID=${ANILIST_CLIENT_ID}" > Config.xcconfig
 fi
