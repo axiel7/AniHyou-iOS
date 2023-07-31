1999941802860ebc6e5f86610dcceb90579d62e4
axiel7 <axiel7lopez@gmail.com>
2023-07-31 22:30:45 +0900

    fix spoiler tag color on macOS

--- AniHyou/Views/Media/MediaTagItemView.swift
+++ AniHyou/Views/Media/MediaTagItemView.swift
@@ -11,12 +11,15 @@ import AniListAPI
 
 private extension MediaTagItemView {
     @ViewBuilder
-    var spoilerOverlay: some View {
+    func spoilerOverlay(_ tint: Color) -> some View {
         if tag?.isMediaSpoiler == true {
             if showSpoiler.wrappedValue {
-                RoundedRectangle(cornerRadius: 8).stroke()
+                RoundedRectangle(cornerRadius: 8)
+                    .stroke()
+                    .foregroundColor(tint)
             } else {
                 RoundedRectangle(cornerRadius: 8)
+                    .foregroundColor(tint)
             }
         } else {
             EmptyView()
@@ -29,6 +32,11 @@ struct MediaTagItemView: View {
     var tag: MediaDetailsQuery.Data.Media.Tag?
     var showSpoiler: Binding<Bool>
     @State private var showTagDescription = false
+    @Environment(\.colorScheme) private var colorScheme
+    //for some reason on macOS Color.primary doesn't use the same color for text and shapes
+    var tint: Color {
+        colorScheme == .light ? .black : .white
+    }
 
     var body: some View {
         HStack {
@@ -41,8 +49,9 @@ struct MediaTagItemView: View {
         .padding(.vertical, 8)
         .background(.thickMaterial, in: RoundedRectangle(cornerRadius: 8))
         .overlay {
-            spoilerOverlay
+            spoilerOverlay(tint)
         }
+        .foregroundColor(tint)
         .onTapGesture {
             if tag?.isMediaSpoiler == true {
                 if showSpoiler.wrappedValue {
