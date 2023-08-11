6f7807ed7940b0d4ebad3443770e7e3f61838bad
axiel7 <axiel7lopez@gmail.com>
2023-08-11 09:45:07 +0900

    models refactor: UserDefaultsUtils

--- AniHyou/Utils/ModelUtils.swift
+++ AniHyou/Utils/ModelUtils.swift
@@ -42,3 +42,10 @@ func someIfNotEmpty(_ value: String?) -> GraphQLNullable<String> {
     if value!.isEmpty { return .none }
     return .some(value!)
 }
+
+extension Collection {
+    /// Returns the element at the specified index if it is within bounds, otherwise nil.
+    subscript(safe index: Index) -> Element? {
+        return indices.contains(index) ? self[index] : nil
+    }
+}
--- AniHyou/Utils/UserDefaultsUtils.swift
+++ AniHyou/Utils/UserDefaultsUtils.swift
@@ -40,10 +40,3 @@ func refreshUserIdAndOptions() {
         }
     }
 }
-
-extension Collection {
-    /// Returns the element at the specified index if it is within bounds, otherwise nil.
-    subscript(safe index: Index) -> Element? {
-        return indices.contains(index) ? self[index] : nil
-    }
-}
