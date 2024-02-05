a8a014bdc4f7bab5c3e168cff432abc86c44b8b7
axiel7 <axiel7lopez@gmail.com>
2024-02-05 13:12:27 +0900

    fix status selection

--- AniHyou/Screens/MediaList/Views/MediaListStatusView.swift
+++ AniHyou/Screens/MediaList/Views/MediaListStatusView.swift
@@ -12,21 +12,23 @@ struct MediaListStatusView: View {
 
     let mediaType: MediaType
     var userId: Int?
-    @AppStorage(LIST_STATUS_KEY) private var selection: MediaListStatus = .current
+    @AppStorage(LIST_STATUS_KEY) private var selection: MediaListStatus?
     @State private var showingMediaDetails = false
     @State private var mediaId = 0
 
     var body: some View {
         NavigationSplitView {
-            List(MediaListStatus.allCases, id: \.self, selection: Binding($selection)) { status in
+            List(MediaListStatus.allCases, id: \.self, selection: $selection) { status in
                 Label(status.localizedName, systemImage: status.systemImage)
             }//:List
             .navigationTitle(mediaType == .anime ? "Anime List" : "Manga List")
         } detail: {
-            NavigationStack {
-                MediaListView(type: mediaType, status: selection, userId: userId)
-                    .id(selection)
-                    .addOnOpenMediaUrl($showingMediaDetails, $mediaId)
+            if let selection {
+                NavigationStack {
+                    MediaListView(type: mediaType, status: selection, userId: userId)
+                        .id(selection)
+                        .addOnOpenMediaUrl($showingMediaDetails, $mediaId)
+                }
             }
         }//:NavigationSplitView
     }
