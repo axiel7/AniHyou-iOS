a88973d2050f644c4930753bbf4db2e1f219203c
axiel7 <axiel7lopez@gmail.com>
2024-09-20 12:26:29 +0200

    feat: italian translation

--- AniHyou/Screens/Settings/TranslationCredits.swift
+++ AniHyou/Screens/Settings/TranslationCredits.swift
@@ -11,6 +11,7 @@ private let translations = [
     "ar_SA": "yngams, Hussain889, Hussain69o, WhiteCanvas, eyadalomar, sakugaky, Comikazie, mlvin, bobteen1",
     "az_AZ": "oolyvi",
     "in_ID": "Clxf12, naturbrilian",
+    "it_IT": "maicol07, DomeF, fraluc06",
     "ja_JP": "axiel7",
     "pl_PL": "xiggeush, YOGI_AOGI",
     "pt_BR": "Crono0, Torti, Ratolino, RickyM7, SamOak",
