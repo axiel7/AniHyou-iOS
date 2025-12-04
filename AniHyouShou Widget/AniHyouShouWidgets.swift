65774795e0fae501f555d3bccab14359305d0c85
axiel7 <axiel7lopez@gmail.com>
2025-12-04 12:55:05 +0100

    feat(watchOS): launch app complication

---
+++ AniHyouShou Widget/AniHyouShouWidgets.swift
@@ -0,0 +1,16 @@
+//
+//  AniHyouShou_WidgetBundle.swift
+//  AniHyouShou Widget
+//
+//  Created by Axel Lopez on 4/12/25.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct AniHyouShouWidgets: WidgetBundle {
+    var body: some Widget {
+        LaunchAppWidget()
+    }
+}
---
+++ AniHyouShou Widget/LaunchAppWidget.swift
@@ -0,0 +1,62 @@
+//
+//  LaunchAppWidget.swift
+//  AniHyouShou Widget
+//
+//  Created by Axel Lopez on 4/12/25.
+//
+
+import WidgetKit
+import SwiftUI
+
+private struct Provider: TimelineProvider {
+    func placeholder(in context: Context) -> LaunchEntry {
+        LaunchEntry(date: Date.now)
+    }
+    
+    func getSnapshot(in context: Context, completion: @escaping @Sendable (LaunchEntry) -> Void) {
+        completion(LaunchEntry(date: Date.now))
+    }
+    
+    func getTimeline(in context: Context, completion: @escaping @Sendable (Timeline<LaunchEntry>) -> Void) {
+        completion(Timeline(entries: [LaunchEntry(date: Date.now)], policy: .never))
+    }
+}
+
+private struct LaunchEntry: TimelineEntry {
+    var date: Date
+}
+
+private struct LaunchAppWidgetView: View {
+    private let launchURL = URL(string: "anihyou://launch")!
+    
+    var body: some View {
+        Link(destination: launchURL) {
+            Image("AniHyou_frg")
+                .resizable()
+                .scaledToFit()
+        }
+        .containerBackground(.background, for: .widget)
+    }
+}
+
+struct LaunchAppWidget: Widget {
+    
+    let kind = "AniHyou_Launch_App_Widget"
+    
+    var body: some WidgetConfiguration {
+        StaticConfiguration(
+            kind: kind,
+            provider: Provider()
+        ) { _ in
+            LaunchAppWidgetView()
+        }
+        .configurationDisplayName("Launch AniHyou")
+        .supportedFamilies([.accessoryCircular])
+    }
+}
+
+#Preview(
+    as: .accessoryCircular,
+    widget: { LaunchAppWidget() },
+    timeline: { LaunchEntry(date: Date.now) }
+)
