f100470f51a47eb015b763e41099544935704d6e
Axel <axiel7lopez@gmail.com>
2026-08-22 23:57:24 +0200

    chore(deps): apollo 2.4.0

--- AniListAPI/Package.swift
+++ AniListAPI/Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     .library(name: "AniListAPI", targets: ["AniListAPI"]),
   ],
   dependencies: [
-    .package(url: "https://github.com/apollographql/apollo-ios", exact: "2.3.0"),
+    .package(url: "https://github.com/apollographql/apollo-ios", exact: "2.4.0"),
   ],
   targets: [
     .target(
